08e76ebe6f1892e13da432635aa9c7e1007ef138
unknown <rohitp@techepoch.com>
2020-04-23 10:36:57 +0530

    msg

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={			
+	'ApiUrl':"http://appetics.online/driver/api",		
+	'DialogDefaultTitle':"DriverApp",
+	'mapboxToken' : '',
+	'APIHasKey':"",
+	'debug': false
+};
